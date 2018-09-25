11174e88cab17c490a0cbda71fdd613e17fd67b6
rautelaanirudh <anirudhsinghrautela@gmail.com>
2018-09-25 15:10:54 +0530

    testing again

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Topic, :type => :model do
   it "is not valid without a title" do
     topic = Topic.new(title: nil)
-    #expect(topic).to_not be_valid
-    expect(topic).to be_valid
+    expect(topic).to_not be_valid
+    #expect(topic).to be_valid
   end
 
 end
